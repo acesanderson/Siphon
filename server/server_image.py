a3587123acd199663d0810cf01348c3f69252bcd
bianders <phat@biden.co.uk>
2025-06-15 21:27:22 -0500

    merge

--- server/server_image.py
+++ server/server_image.py
@@ -2,6 +2,7 @@
 This adapts our image analysis code to work with the ContextCall object.
 """
 
+from Chain.message.convert_image import convert_image
 from Siphon.server.context_call import ContextCall
 from Siphon.ingestion.image.image import describe_image
 from pathlib import Path
@@ -15,21 +16,6 @@ def describe_image_ContextCall(context_call: ContextCall) -> str:
     """
     # Decode base64 data to bytes
     base64_bytes = base64.b64decode(context_call.base64_data)
-    # Create a temporary file path
-    temp_file = tempfile.NamedTemporaryFile(
-        delete=False, suffix=f".{context_call.extension}"
-    )
-    temp_file.write(base64_bytes)
-    temp_file.close()
-    temp_path = Path(temp_file.name)
-    # See if we need to convert to mp3    try:
-    try:
-        transcript = describe_image(temp_path)
-        if not transcript:
-            raise ValueError("No transcript found for the audio file.")
-        return str(transcript)
-    finally:
-        temp_path.unlink(missing_ok=True)
 
 
 if __name__ == "__main__":
